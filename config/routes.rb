db7a9e150a49b4f2cfb27a45f1e5cad3c5991245
John McCaffrey <John.McCaffrey@gmail.com>
2011-03-15 16:55:50 -0500

    adding simple_captcha to user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.simple_captcha '/simple_captcha/:action', :controller => 'simple_captcha'
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "users"
 
   # See how all your routes lay out with "rake routes"
 
