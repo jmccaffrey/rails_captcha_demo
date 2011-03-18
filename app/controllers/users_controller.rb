e79e00daab94172091c4e3cdaa72494b9e706b05
John McCaffrey <John.McCaffrey@gmail.com>
2011-03-18 09:06:37 -0500

    clean up respond to block

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,25 +41,17 @@ class UsersController < ApplicationController
   # POST /users.xml
   def create
     @user = User.new(params[:user])
-    # respond_to do |format|
       if simple_captcha_valid? 
         if @user.save
           flash[:notice] = 'User was successfully created.'
-          # format.html { 
             redirect_to(@user) 
-            # }
-          # format.xml  { render :xml => @user, :status => :created, :location => @user }
         else
-          # format.html { 
             render :action => "new" 
-            # }
-          # format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
         end
       else  
         flash[:error] = 'Captcha no good' 
         render :action => "new" 
       end
-    # end
   end
 
   # PUT /users/1
